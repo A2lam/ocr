16fd3b9186805f3b942df46625d1b7dce80ca840
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-11 00:18:41 +0200

    Création de la class NumberCorresp retranscrivant un nombre au résultat

---
+++ src/main/java/NumberCorrespondence.java
@@ -0,0 +1,76 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class NumberCorrespondence
+{
+    private int number;
+    private ArrayList<String> numberCorrespondenceArray;
+
+    public NumberCorrespondence(int number) throws IllegalArgumentException
+    {
+        if ((number < 1) || (number > 9))
+            throw new IllegalArgumentException();
+
+        this.number = number;
+        this.numberCorrespondenceArray = new ArrayList<String>();
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public ArrayList<String> getNumberCorrespondenceArray()
+    {
+        switch (this.number)
+        {
+            case 1:
+                this.numberCorrespondenceArray.add("  ");
+                this.numberCorrespondenceArray.add(" |");
+                this.numberCorrespondenceArray.add(" |");
+                break;
+            case 2:
+                this.numberCorrespondenceArray.add(" _");
+                this.numberCorrespondenceArray.add(" _|");
+                this.numberCorrespondenceArray.add("|_ ");
+                break;
+            case 3:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add(" _|");
+                this.numberCorrespondenceArray.add(" _|");
+                break;
+            case 4:
+                this.numberCorrespondenceArray.add("  ");
+                this.numberCorrespondenceArray.add("|_|");
+                this.numberCorrespondenceArray.add("  |");
+                break;
+            case 5:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add("|_ ");
+                this.numberCorrespondenceArray.add(" _|");
+                break;
+            case 6:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add("|_ ");
+                this.numberCorrespondenceArray.add("|_|");
+                break;
+            case 7:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add("  |");
+                this.numberCorrespondenceArray.add("  |");
+                break;
+            case 8:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add("|_|");
+                this.numberCorrespondenceArray.add("|_|");
+                break;
+            case 9:
+                this.numberCorrespondenceArray.add(" _ ");
+                this.numberCorrespondenceArray.add("|_|");
+                this.numberCorrespondenceArray.add(" _|");
+                break;
+        }
+
+        return this.numberCorrespondenceArray;
+    }
+}

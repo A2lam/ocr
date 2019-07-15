5e518dd564458751ce3f039a83d8ea5bd77104c4
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-15 13:44:24 +0200

    Écriture du fichier de test du pasrser

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParserTest
+{
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    void setUpStreams()
+    {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    void coded123456789_should_display_123456789()
+    {
+        ArrayList<String> encodedMessage = new ArrayList<String>();
+        encodedMessage.add("    _  _     _  _  _  _  _ ");
+        encodedMessage.add(" |  _| _||_||_ |_   ||_||_|");
+        encodedMessage.add(" | |_  _|  | _||_|  ||_| _|");
+        encodedMessage.add("                           ");
+
+        Parser numberParser = new Parser(encodedMessage);
+        numberParser.parse();
+        assertEquals("123456789\n", outContent.toString());
+    }
+
+    @AfterEach
+    void restoreStreams()
+    {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+}
